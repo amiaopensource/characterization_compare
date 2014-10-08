5f8d3436175ded5da185d10d18921f038a2d4d94
Ben Fino-Radin <bfinoradin@gmail.com>
2014-10-08 12:34:46 -0400

    dictionary mode for ffprobe

--- vidcomp.py
+++ vidcomp.py
@@ -4,18 +4,16 @@ import argparse, os, subprocess
 from subprocess import call
 import xml.etree.ElementTree as ET
 
-
 parser = argparse.ArgumentParser(description="Python tool for comparing the output of video characterization tools")
 parser.add_argument('-i', '--input', type=str, required=True, help='The full path to the file you want to analyze.')
 args = parser.parse_args()
 
-# def get_version():
-# 	cmd = ['ffprobe', '-version']
-# 	p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-# 	out = p.communicate()
-# 	print out[:15]
 
-# get_version()
+# ===============
+# FFPROBE attributes
+ffprobe_format = {'format_name':None, 'format_long_name':None, 'size':None, 'duration':None, 'bit_rate':None}
+ffprobe_video_track = {'codec_name':None, 'codec_tag_string':None, 'profile':None, 'display_aspect_ratio':None, 'r_frame_rate':None, 'pix_fmt':None}
+ffprobe_audio_track = {'codec_name':None, 'codec_tag_string':None, 'sample_rate':None, 'bits_per_sample':None, 'channels':None}
 
 def probe_file(filename):
     cmnd = ['ffprobe', '-show_format', '-show_streams', '-show_error', '-show_versions', '-print_format', 'xml', filename]
@@ -23,26 +21,18 @@ def probe_file(filename):
     out, err =  p.communicate()
     root = ET.fromstring(out)
 
-    # ===============
-    # FFPROBE attributes
-    ffprobe_format = ['format_name', 'format_long_name', 'size', 'duration', 'bit_rate']
-    ffprobe_video_track = ['codec_name', 'codec_tag_string', 'profile', 'display_aspect_ratio', 'r_frame_rate', 'pix_fmt']
-    ffprobe_audio_track = ['codec_name', 'codec_tag_string', 'sample_rate', 'bits_per_sample', 'channels']
-
     for elem in root.iterfind('format'):
-        print "\n=======> container"
-    	for item in ffprobe_format:
-    		print item + ": " + elem.attrib[item]
+        for item in ffprobe_format:
+            ffprobe_format[item] = elem.attrib[item]
 
     for stream in root.iter('stream'):
     	if stream.attrib['codec_type'] == "video":
-            print "\n=======> video stream(s)"
             for item in ffprobe_video_track:
-	    		print item + ": " + stream.attrib[item]
+                ffprobe_video_track[item] = stream.attrib[item]
+
         elif stream.attrib['codec_type'] == "audio":
-            print "\n=======> audio stream(s)"
             for item in ffprobe_audio_track:
-                print item + ": " + stream.attrib[item]
+                ffprobe_audio_track[item] = stream.attrib[item]
 
 def mediainfo_file(filename):
     cmnd = ['mediainfo', '-f', '--language=raw', '--Output=XML', filename]
@@ -65,3 +55,7 @@ print "FFPROBE output"
 probe_file(args.input)
 print "---------------------------------------"
 mediainfo_file(args.input)
+print "container",ffprobe_format
+print "video",ffprobe_video_track
+print "audio",ffprobe_audio_track
+
