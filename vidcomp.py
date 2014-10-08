6fa881068f4c9c1393dc7ce52e41ebb87ed46a06
Ben Fino-Radin <bfinoradin@gmail.com>
2014-10-08 10:40:27 -0400

    stuff

--- vidcomp.py
+++ vidcomp.py
@@ -1,20 +1,39 @@
 #!/usr/bin/env python
 
-import argparse, os
+import argparse, os, subprocess
+from subprocess import call
+import xml.etree.ElementTree as ET
 
-'''
-todo
-
-- outputs XML for each tool
-- use xpath to identify each value
-
-argumenet for file
-
-'''
 
 parser = argparse.ArgumentParser(description="Python tool for comparing the output of video characterization tools")
 parser.add_argument('-i', '--input', type=str, required=True, help='The full path to the file you want to analyze.')
 args = parser.parse_args()
 
-# function that runs mediainfo, exif, ffprobe
 
+def probe_file(filename):
+    cmnd = ['ffprobe', '-show_format', '-show_streams', '-show_error', '-show_versions', '-print_format', 'xml', filename]
+    p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err =  p.communicate()
+    # print out
+    root = ET.fromstring(out)
+    # print root
+    # print root.tag
+    # print root.attrib
+    print root.iter
+    # for child in root:
+    # 	print child.tag, child.attrib
+
+
+'''
+list of children
+
+ffprobe: //format[@format_name]
+ffprobe: //format[@format_long_name]
+ffprobe: //format[@size]
+ffprobe: //format[@duration]
+ffprobe: //format[@bit_rate]
+
+
+'''
+
+probe_file(args.input)
