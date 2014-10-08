9b039e3f4dca349535e7545da032740f998ec00f
iamdamosuzuki <iownafordprobe@gmail.com>
2014-10-08 15:33:07 -0400

    audio tracks from medianfo in CSV

--- vidcomp.py
+++ vidcomp.py
@@ -133,6 +133,11 @@ c.writerow(["ffprobe",ffprobe_video_track['codec_name'],ffprobe_video_track['cod
 for i in range(1,len(mediainfo_video_track_master)):
     miList = mediainfo_video_track_master[i]
     c.writerow(["Media Info",miList['Format'],miList['CodecID'],miList['Format_Profile'],miList['DisplayAspectRatio'],miList['FrameRate'],miList['ChromaSubsampling'],miList['Resolution'],miList['ColorSpace']])
+c.writerow(["Audio track 1","Audio Codec","Codec ID","Codec Profile","Sampling rate","Bit Depth","Number of Channels"])
+c.writerow(["ffprobe",ffprobe_audio_track['codec_name'],ffprobe_audio_track['codec_tag_string'],'N/A',ffprobe_audio_track['sample_rate'],ffprobe_audio_track['bits_per_sample'],ffprobe_audio_track['channels']])
+for i in range(1,len(mediainfo_audio_track_master)):
+    miList = mediainfo_audio_track_master[i]
+    c.writerow(["Media Info",miList['Format'],miList['CodecID'],miList['Format_Profile'],miList['SamplingRate'],miList['AudioBitsPerSample'],miList['Channel_s_']])
 
 
 
